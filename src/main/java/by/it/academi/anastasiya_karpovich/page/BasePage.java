53d97be07bcd1e69425025495e708c080e7fde8f
Anastasiya Karpovich <nastasya.karpovich@gmail.com>
2022-08-30 00:10:02 +0300

    add basepage

---
+++ src/main/java/by/it/academi/anastasiya_karpovich/page/BasePage.java
@@ -0,0 +1,39 @@
+package by.it.academi.anastasiya_karpovich.page;
+
+import by.it.academi.anastasiya_karpovich.driver.DriverSingleton;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+    protected static final Logger logger = LogManager.getLogger();
+    protected WebDriver driver;
+    protected final String BASE_URL = "https://www.avis.com/en/home";
+    protected final int WAIT_TIMEOUT_SECONDS = 15;
+
+    protected BasePage() {
+        driver = DriverSingleton.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    protected WebElement waitForElementToBeClickable(WebElement webElement) {
+        return new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS)).until(ExpectedConditions
+                .elementToBeClickable(webElement));
+    }
+
+    protected WebElement waitForVisibilityOfElement(WebElement webElement) {
+        return new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS)).until(ExpectedConditions
+                .visibilityOf(webElement));
+    }
+    protected WebElement waitForVisibilityOfElement(By webElementLocator) {
+        return new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated(webElementLocator));
+    }
+}
